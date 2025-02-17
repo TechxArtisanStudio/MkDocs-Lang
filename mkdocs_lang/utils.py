1c9bb5c4a31af2eddeb252c77ad2d25e5b56baaa
Billy-C1 <billy@techxartisan.com>
2025-02-17 12:00:23 +0800

    Refactor: Improve project path detection and site path handling

--- mkdocs_lang/utils.py
+++ mkdocs_lang/utils.py
@@ -1,4 +1,5 @@
 import os
+import yaml
 
 def get_main_project_path(provided_path=None):
     """
@@ -20,8 +21,11 @@ def find_main_project_path():
     """
     current_path = os.getcwd()
     while current_path != os.path.dirname(current_path):  # Traverse up to the root
-        if os.path.exists(os.path.join(current_path, 'mkdocs-lang.yml')):
-            return current_path
+        mkdocs_lang_yml_path = os.path.join(current_path, 'mkdocs-lang.yml')
+        if os.path.exists(mkdocs_lang_yml_path):
+            with open(mkdocs_lang_yml_path, 'r') as f:
+                config = yaml.safe_load(f)
+                return config.get('main_project_path', current_path)
         current_path = os.path.dirname(current_path)
     
     print("Error: mkdocs-lang.yml not found. Please specify the --project path.")
